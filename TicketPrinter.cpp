f3f0cee68e786c5755cdc0f5612289f2c8ef7f33
nathancread <nathancread@gmail.com>
2022-04-17 01:50:20 -0500

    ticket printer class completed

---
+++ TicketPrinter.cpp
@@ -0,0 +1,128 @@
+#include "TicketPrinter.h"
+
+TicketPrinter::TicketPrinter()
+{
+}
+
+StudentTicket* TicketPrinter::getTicketFeild()
+{
+    return this->ticket;
+}
+void TicketPrinter::SetupTicket(StudentTicket* ticket)
+{
+     this->ticket = ticket;
+}
+void TicketPrinter::printUniversty()
+{
+    StudentTicket *t = this->getTicketFeild();
+    string s = t->getUniversty();
+    printf("Universty %s ", s);
+}
+void TicketPrinter::printSurname()
+{
+    StudentTicket *t = this->getTicketFeild();
+    string s = t->getSurname();
+    printf("Surname %s ", s);
+}
+void TicketPrinter::printStudyType()
+{
+    StudentTicket *t = this->getTicketFeild();
+    string s = t->getStudyType();
+    printf("StudyType %s ", s);
+}
+void TicketPrinter::printStructuredUnit()
+{
+    StudentTicket *t = this->getTicketFeild();
+    string s = t->getStructuredUnit();
+    printf("StructuredUnit %s ", s);
+}
+void TicketPrinter::printSpecialization()
+{
+    StudentTicket *t = this->getTicketFeild();
+    string s = t->getSpecialization();
+    printf("Specialization %s ", s);
+}
+void TicketPrinter::printRectorCredentials()
+{
+    StudentTicket *t = this->getTicketFeild();
+    string s = t->getRectorCredentials();
+    printf("RectorCredentials %s ", s);
+}
+void TicketPrinter::printPart()
+{
+    StudentTicket *t = this->getTicketFeild();
+    string s = t->getPart();
+    printf("Part %s ", s);
+}
+void TicketPrinter::printName()
+{
+    StudentTicket *t = this->getTicketFeild();
+    string s = t->getName();
+    printf("Name %s ", s);
+}
+void TicketPrinter::printIssueDateYear()
+{
+    StudentTicket *t = this->getTicketFeild();
+    int i = t->getIssueDateYear();
+    printf("IssueDateYear %d ", i);
+}
+void TicketPrinter::printIssueDateMonth()
+{
+    StudentTicket *t = this->getTicketFeild();
+    int i = t->getIssueDateMonth();
+    printf("IssueDateMonth %d ", i);
+}
+void TicketPrinter::printIssueDateDay()
+{
+    StudentTicket *t = this->getTicketFeild();
+    int i = t->getIssueDateDay();
+    printf("IssueDateDay %d ", i);
+}
+void TicketPrinter::printId()
+{
+    StudentTicket *t = this->getTicketFeild();
+    int i = t->getId();
+    printf("Id %d ", i);
+}
+void TicketPrinter::printGroup()
+{
+    StudentTicket *t = this->getTicketFeild();
+    string s = t->getGroup();
+    printf("Group %d ", s);
+}
+void TicketPrinter::printFatherName()
+{
+    StudentTicket *t = this->getTicketFeild();
+    string s = t->getFatherName();
+    printf("FatherName %d ", s);
+}
+void TicketPrinter::printFaculty()
+{
+    StudentTicket *t = this->getTicketFeild();
+    string s = t->getFaculty();
+    printf("Faculty %d ", s);
+}
+void TicketPrinter::printExperationDateYear()
+{
+    StudentTicket *t = this->getTicketFeild();
+    int i = t->getExplorationDateYear();
+    printf("ExplorationDateYear %d ", i);
+}
+void TicketPrinter::printExperationDateMonth()
+{
+    StudentTicket *t = this->getTicketFeild();
+    int i = t->getExperationDateMonth();
+    printf("ExperationDateMonth %d ", i);
+}
+void TicketPrinter::printExperationDateDay()
+{
+    StudentTicket *t = this->getTicketFeild();
+    int i = t->getExperationDateDay();
+    printf("ExperationDateDay %d ", i);
+}
+void TicketPrinter::printEntranceYear()
+{
+    StudentTicket *t = this->getTicketFeild();
+    int i = t->getEntranceYear();
+    printf("EntranceYear %d ", i);
+}
---
+++ TicketPrinter.h
@@ -0,0 +1,33 @@
+#include "common.h"
+#include "StudentTicket.h"
+
+class TicketPrinter
+{
+private:
+    string BarCode;
+    StudentTicket* ticket;
+    
+public:
+    TicketPrinter();
+    void SetupTicket(StudentTicket* s );
+    StudentTicket* getTicketFeild();
+    void printUniversty();
+    void printSurname();
+    void printStructuredUnit();
+    void printSpecialization();
+    void printRectorCredentials();
+    void printPart();
+    void printName();
+    void printIssueDateYear();
+    void printIssueDateMonth();
+    void printIssueDateDay();
+    void printId();
+    void printGroup(); 
+    void printStudyType(); 
+    void printFatherName();
+    void printFaculty(); 
+    void printExperationDateYear();
+    void printExperationDateMonth();
+    void printExperationDateDay();
+    void printEntranceYear();
+};
