e8efe5ec38796ac3fdac02897bd94fb4fe097fa0
nathancread <nathancread@gmail.com>
2022-04-27 15:25:27 -0500

    revised main

--- main.cpp
+++ main.cpp
@@ -3,17 +3,25 @@
 
 int main(int argc, char *argv[])
 {
-    if(argc != 3)
+    if (argc != 3) //always break if too many or too few inputs
     {
         puts("usage: x Part Id");
         exit();
     }
 
-    bool badInput;
-    string part = argv[1]; 
-    int id = stoi(argv[2]); 
+    string part = argv[1];
+    int id = 0;
+    try 
+    { 
+        id = stoi(argv[2]); //STOI is considered an unsafe method, try catch required
+    }
+    catch (exception &err)
+    {
+        puts("usage: x Part Id");
+        exit();
+    }
 
-    if (id == 0) // bad input if id is 0
+    if (id == 0 || part == "" ||id<0) // bad input if id is 0
     {
         puts("usage: x Part Id");
     }
@@ -26,6 +34,6 @@ int main(int argc, char *argv[])
         bool gotTicket = t->getTicket();
         delete t;
     }
-    
+
     return 0;
 }
