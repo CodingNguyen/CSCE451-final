bde81401d9db8a71a30aa1c0c7506d8c46fb94ba
nathancread <nathancread@gmail.com>
2022-04-28 00:30:35 -0500

    change spelling mistakes

--- TicketPrinter.cpp
+++ TicketPrinter.cpp
@@ -14,8 +14,8 @@ void TicketPrinter::SetupTicket(StudentTicket *ticket)
 }
 void TicketPrinter::printAll()
 {
-    StudentTicket* t = getTicketFeild();
-    printf("Universty %s ", t->getUniversty().c_str());
+    StudentTicket* t = getTicketField();
+    printf("Universty %s ", t->getUniversity().c_str());
     printf("Surname %s ", t->getSurname().c_str());
     printf("StudyType %s ", t->getStudyType().c_str());
     printf("StructuredUnit %s ", t->getStructuredUnit().c_str());
@@ -27,9 +27,9 @@ void TicketPrinter::printAll()
     printf("IssueDateMonth %d ", t->getIssueDateMonth());
     printf("IssueDateDay %d ", t->getIssueDateDay());
     printf("Id %d ", t->getId());
-    printf("Group %d ", t->getGroup());
-    printf("FatherName %d ", t->getFatherName());
-    printf("Faculty %d ", t->getFaculty());
+    printf("Group %s ", t->getGroup());
+    printf("FatherName %s ", t->getFatherName());
+    printf("Faculty %s ", t->getFaculty());
     printf("ExperationDateYear %d ", t->getExperationDateYear());
     printf("ExperationDateMonth %d ", t->getExperationDateMonth());
     printf("ExperationDateDay %d ", t->getExperationDateDay());
